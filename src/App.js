67e1b45163000fff70d3c41b6825541092e07a03
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-11-17 10:22:47 +0800

    ✨ Update App to call ProductSearch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { DayForecast } from './weatherForecast/DayForecast';
 import './App.css';
+import { forecastData } from './weatherForecast/data';
+import Scoreboard from './cricketScoreboard/Scoreboard';
+import ProductList from './productSearch/productList';
 
-function App() {
+
+const WeatherForecast = () => {
+  return (
+    forecastData.map((item) => <DayForecast day={item.day} conditions={item.conditions} maxTemp={item.maxTemp} wind={item.wind} hourlyTemp={item.hourlyTemp} />)
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <WeatherForecast /> */}
+        {/* <Scoreboard /> */}
+        <ProductList />
       </header>
     </div>
   );
-}
+};
 
 export default App;
