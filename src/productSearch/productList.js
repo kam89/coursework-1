69b45d635536d4c43dbff84abd6c6ffca1dc5150
K M <30794641+kam89@users.noreply.github.com>
2022-11-23 02:09:57 +0800

    :zap: Change filter criteria to checkbox

--- src/productSearch/productList.js
+++ src/productSearch/productList.js
@@ -1,23 +1,63 @@
 import { Component } from "react";
 import Product from "./product";
-import { products, category } from "./data";
+import { products } from "./data";
 
 export default class ProductList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { productList: products };
+    this.state = { productList: products, category: [], filterCriteria: [] };
   }
 
-  onFilter = (e) => {
-    if (e === category.ALL) return this.setState({ productList: products });
-    let filterCriteria = e.toLowerCase();
-    let newList;
-    if (e === category.BOTTOM)
-      newList = products.filter(item => item.category === "pants" || item.category === "skirts");
-    else
-      newList = products.filter(item => item.category === filterCriteria);
-    this.setState({ productList: newList });
+  componentDidMount() {
+    this.getCategory();
+  }
+
+  getCategory = () => {
+    const sortedList = products.sort((a, b) => a.category === b.category ? 0 : a.category ? 1 : -1);
+    let newCategoryList = [];
+    sortedList.forEach((item, index) => {
+      if (newCategoryList.includes(item.category)) return;
+      newCategoryList.push(item.category);
+    });
+    this.setState({ category: newCategoryList });
+  };
+
+  // onFilter = (e) => {
+  //   if (e === this.state.category.ALL) return this.setState({ productList: products });
+  //   let filterCriteria = e.toLowerCase();
+  //   let newList;
+  //   if (e === this.state.category.BOTTOM)
+  //     newList = products.filter(item => item.category === "pants" || item.category === "skirts");
+  //   else
+  //     newList = products.filter(item => item.category === filterCriteria);
+  //   this.setState({ productList: newList });
+  // };
+
+  onFilter = (selectedItem, e) => {
+    if (this.state.filterCriteria.length < 1) {
+      const newList = products.filter(item => item.category === selectedItem);
+      return this.setState({ productList: newList, filterCriteria: [selectedItem] });
+    }
+
+    let newFilterCriteria = [];
+    if (e.target.checked) {
+      newFilterCriteria = [...this.state.filterCriteria, selectedItem];
+    }
+    if (!e.target.checked) {
+      newFilterCriteria = this.state.filterCriteria.slice();
+      const deleteIndex = newFilterCriteria.findIndex((item) => item === selectedItem);
+      const result = newFilterCriteria.splice(deleteIndex, 1);
+    }
+    this.setState({ filterCriteria: newFilterCriteria });
+
+    let newList = [];
+
+    newFilterCriteria.forEach((filter, index) => {
+      const temp = products.filter(item => item.category === filter);
+      newList = [...newList, ...temp];
+    });
+    return this.setState({ productList: newList });
   };
 
   onSearch = (e) => {
@@ -30,9 +70,11 @@ export default class ProductList extends Component {
       <div>
         <div>
           <div style={{ display: "inline", justifyContent: "space-around" }}>
-            {Object.values(category).map((item, index) => {
+            {this.state.category.map((item, index) => {
               return (
-                <button key={index} style={{ marginLeft: 5, display: "inline" }} onClick={() => this.onFilter(item)}>{item}</button>
+                <div style={{ marginLeft: 5, display: "inline" }}>
+                  <input type="checkbox" key={index} onChange={(e) => this.onFilter(item, e)} /><label>{item}</label>
+                </div>
               );
             })}
             <input style={{ alignSelf: "flex-end", marginLeft: 100 }} onChange={this.onSearch}></input>
@@ -47,4 +89,4 @@ export default class ProductList extends Component {
       </div>
     );
   }
-}
+};;
