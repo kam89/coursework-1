a18d5a7bd7b0678286aa872b90ca6a9098302b12
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-11-29 18:20:57 +0800

    ✨ Add DateFactMain

---
+++ src/dateFact/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DateFact } from './DateFact';
+
+export const DateFactMain = () => {
+
+  return (
+    <div>
+      <input />
+      <DateFact month={1} day={23} />
+      {/* <DateFact month={4} day={1} /> */}
+      {/* <DateFact month={12} day={25} /> */}
+    </div>
+  );
+
+};
